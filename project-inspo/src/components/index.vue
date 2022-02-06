b415ead153900ecf10a223272fdaed4ae87ea466
MohaElder <calen0909@hotmail.com>
2022-02-05 17:46:27 -0800

    initialize vue project

---
+++ project-inspo/src/components/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="msg" />
+    <button @click="change">You can also click me to change content</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      msg: "hi",
+    };
+  },
+  methods: {
+    change() {
+      this.msg = "See? Now it's changed";
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  display: grid;
+  grid-gap: 30px;
+  height: 500px;
+}
+</style>
