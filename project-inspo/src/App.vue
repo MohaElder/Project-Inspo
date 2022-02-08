28b2c544c9acbc1c7cfabea4ba35a478fa9cedc7
sophifang <sophiayanfang@gmail.com>
2022-02-07 19:05:42 -0800

    changed msg

--- project-inspo/src/App.vue
+++ project-inspo/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <p>hi</p>
   <index />
 </template>
 
--- project-inspo/src/components/index.vue
+++ project-inspo/src/components/index.vue
@@ -11,7 +11,7 @@ export default {
   name: "index",
   data() {
     return {
-      msg: "hi",
+      msg: "hello",
     };
   },
   methods: {
