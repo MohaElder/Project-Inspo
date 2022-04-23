e8edfec3eb97eb96950b9c1ddb4c8b3f76463051
david tso <davidtso1219@gmail.com>
2022-04-22 17:22:40 -0700

    Added new component, card

---
+++ src/components/card.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="card">
+        <img class="dismiss-x" src="../assets/icons/cross.png" @click="dismiss" />
+        <div class="card-header">
+            {{ card.name }}
+            <div class="card-header-line"></div>
+        </div>
+        <ul class="card-body">
+            <li class="card-item" v-for="(str, index) in card.notes" :key="index">
+            {{ str }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "card",
+
+  props:
+  {
+    note: Object
+  },
+
+  data() {
+    return {
+      card: this.note,
+    };
+  },
+
+  methods: {
+    dismiss() {
+      console.log("dismissed!");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url("../global.css");
+
+.card {
+  position: relative;
+  height: 404px;
+  width: 404px;
+  border: 4px solid var(--orange);
+  border-radius: 21px;
+  padding: 36px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.card-header {
+  height: 50px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  font-family: "Sofia Pro Black";
+  font-size: 30px;
+  color: black;
+}
+
+.card-header-line {
+  width: 100%;
+  height: 3px;
+  background: var(--orange);
+  margin-top: 5px;
+  border-radius: 21px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.card-item {
+  margin: 5px 0;
+  padding: 10px 0;
+  text-align: start;
+  font-size: 30px;
+  font-family: "Sofia Pro Medium";
+  color: black;
+}
+
+.dismiss-x {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  height: 40px;
+}
+
+.dismiss-x:hover {
+  cursor: pointer;
+}
+</style>
--- src/components/note.vue
+++ src/components/note.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="note">
     <div class="blue semi-title">sticky notes</div>
-    <div class="cards-container">
-      <div v-for="note in notes" :key="note.name" class="card">
-        <img class="dismiss-x" src="../assets/icons/cross.png" @click="dismiss" />
-        <div class="card-header">
-          {{ note.name }}
-          <div class="card-header-line"></div>
-        </div>
-        <ul class="card-body">
-          <li class="card-item" v-for="(str, index) in note.notes" :key="index">
-            {{ str }}
-          </li>
-        </ul>
-      </div>
+    <div class="cards-container" v-if="notes.length > 0">
+      <img class="card-control-btn" src="../assets/icons/prev_btn.png" @click="go_next" />
+      <card :note="notes[card_index]"/>
+      <img class="card-control-btn" src="../assets/icons/next_btn.png" @click="go_next" />
     </div>
+    <div v-else> You don't have any sticky notes. </div>
   </div>
 </template>
 
 <script>
 import { User } from "../middleware/data.js";
+import card from "./card.vue";
 
 export default {
   name: "note",
+  components: {
+    card,
+  },
 
   props:
   {
@@ -31,13 +27,12 @@ export default {
 
   data() {
     return {
-      notes: this.user.notes
+      notes: this.user.notes,
+      card_index: 0
     };
   },
+
   methods: {
-    dismiss() {
-      console.log("dismissed!");
-    },
   },
 };
 </script>
@@ -53,60 +48,18 @@ export default {
 }
 
 .cards-container {
-
-}
-
-.card {
-  position: relative;
-  height: 404px;
-  width: 404px;
-  border: 4px solid var(--orange);
-  border-radius: 21px;
-  padding: 36px;
+  margin-top: 20px;
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  background-color: white;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
 }
 
-.card-header {
-  height: 50px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  text-align: start;
-  font-family: "Sofia Pro Black";
-  font-size: 30px;
-  color: black;
+.card-control-btn {
+  height: 96px;
+  width: 96px;
+  margin: auto 20px;
 }
 
-.card-header-line {
-  width: 100%;
-  height: 3px;
-  background: var(--orange);
-  margin-top: 5px;
-  border-radius: 21px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-}
-
-.card-item {
-  margin: 10px 0;
-  padding: 10px 0;
-  text-align: start;
-  font-size: 30px;
-  font-family: "Sofia Pro Medium";
-  color: black;
-}
-
-.dismiss-x {
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  height: 40px;
-}
-
-.dismiss-x:hover {
+.card-control-btn:hover {
   cursor: pointer;
 }
 </style>
