2638496ecc8605e909b1f33347d7d79c08c789e9
MohaElder <calen0909@hotmail.com>
2022-05-20 15:01:18 -0700

    deploy

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'animate.css';
-
-// Vue.prototype.$playTime = "0";
-// Vue.prototype.$workTime = "0";
-
-createApp(App).mount('#app')
